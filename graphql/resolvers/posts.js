2f1fe8efb136c41a5cc6babca57beb92d7c4d3cb
Helloiain <35956118+Helloiain@users.noreply.github.com>
2022-03-01 14:57:14 -0600

    add comment and like functunality

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../utils/check-auth");
@@ -54,6 +54,27 @@ module.exports = {
 			} catch (err) {
 				throw new Error(err);
 			}
+		},
+		async likePost(_, { postId }, context) {
+			const { username } = checkAuth(context);
+
+			const post = await Post.findById(postId);
+			if (post) {
+				if (post.likes.find((like) => like.username === username)) {
+					// Post already lieked, unlike it
+					post.likes = post.likes.filter((like) => like.username !== username);
+					await post.save();
+				} else {
+					// Not liked, like it
+					post.likes.push({
+						username,
+						createdAt: new Date().toDateString(),
+					});
+				}
+
+				await post.save();
+				return post;
+			} else throw new UserInputError("Post not found");
 		},
 	},
 };
