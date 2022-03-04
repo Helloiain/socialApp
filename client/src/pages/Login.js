55fff8a5e43e95fbf7ead35df3f2104d0f01b6ec
Helloiain <35956118+Helloiain@users.noreply.github.com>
2022-03-04 13:12:28 -0600

    login and register pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,83 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Button } from "semantic-ui-react";
+import { gql, useMutation } from "@apollo/client";
 
-function Login() {
+import { useForm } from "../utils/hooks";
+
+function Login(props) {
+	const navigate = useNavigate();
+	const [errors, setErrors] = useState({});
+
+	const { onChange, onSubmit, values } = useForm(loginUserCallback, {
+		username: "",
+		password: "",
+	});
+
+	const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+		update(_, result) {
+			console.log(result);
+			navigate("/");
+		},
+		onError(err) {
+			setErrors(err.graphQLErrors[0].extensions.errors);
+		},
+		variables: values,
+	});
+
+	function loginUserCallback() {
+		loginUser();
+	}
+
 	return (
-		<div>
-			<h1>Login Page</h1>
+		<div className="form-container">
+			<Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
+				<h1>Login</h1>
+				<Form.Input
+					label="username"
+					placeholder="Username.."
+					name="username"
+					value={values.username}
+					error={errors.username ? true : false}
+					onChange={onChange}
+				/>
+				<Form.Input
+					label="password"
+					placeholder="password.."
+					name="password"
+					type="password"
+					value={values.password}
+					error={errors.password ? true : false}
+					onChange={onChange}
+				/>
+				{/* semantic-ui Button throws error so i used custom styling */}
+				<Button color="blue" type="submit">
+					Login
+				</Button>
+			</Form>
+			{Object.keys(errors).length > 0 && (
+				<div className="ui error message">
+					<ul className="list">
+						{Object.values(errors).map((value) => (
+							<li key={value}>{value}</li>
+						))}
+					</ul>
+				</div>
+			)}
 		</div>
 	);
 }
+
+const LOGIN_USER = gql`
+	mutation login($username: String!, $password: String!) {
+		login(username: $username, password: $password) {
+			id
+			email
+			username
+			createdAt
+			token
+		}
+	}
+`;
 
 export default Login;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,113 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Button } from "semantic-ui-react";
+import { gql, useMutation } from "@apollo/client";
 
-function Register() {
+import { useForm } from "../utils/hooks";
+
+function Register(props) {
+	const navigate = useNavigate();
+	const [errors, setErrors] = useState({});
+
+	const { onChange, onSubmit, values } = useForm(registerUser, {
+		username: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
+
+	const [addUser, { loading }] = useMutation(REGISTER_USER, {
+		update(_, result) {
+			navigate("/");
+		},
+		onError(err) {
+			setErrors(err.graphQLErrors[0].extensions.errors);
+		},
+		variables: values,
+	});
+
+	function registerUser() {
+		addUser();
+	}
+
 	return (
-		<div>
-			<h1>Register Page</h1>
+		<div className="form-container">
+			<Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
+				<h1>Register</h1>
+				<Form.Input
+					label="username"
+					placeholder="Username.."
+					name="username"
+					value={values.username}
+					error={errors.username ? true : false}
+					onChange={onChange}
+				/>
+				<Form.Input
+					label="email"
+					placeholder="email.."
+					name="email"
+					value={values.email}
+					error={errors.email ? true : false}
+					onChange={onChange}
+				/>
+				<Form.Input
+					label="password"
+					placeholder="password.."
+					name="password"
+					type="password"
+					value={values.password}
+					error={errors.password ? true : false}
+					onChange={onChange}
+				/>
+				<Form.Input
+					label="confirm password"
+					placeholder="confirm password.."
+					name="confirmPassword"
+					type="password"
+					value={values.confirmPassword}
+					error={errors.confirmPassword ? true : false}
+					onChange={onChange}
+				/>
+				{/* semantic-ui Button throws error so i used custom styling */}
+				<Button color="blue" type="submit">
+					Register
+				</Button>
+			</Form>
+			{Object.keys(errors).length > 0 && (
+				<div className="ui error message">
+					<ul className="list">
+						{Object.values(errors).map((value) => (
+							<li key={value}>{value}</li>
+						))}
+					</ul>
+				</div>
+			)}
 		</div>
 	);
 }
+
+const REGISTER_USER = gql`
+	mutation register(
+		$username: String!
+		$email: String!
+		$password: String!
+		$confirmPassword: String!
+	) {
+		register(
+			registerInput: {
+				username: $username
+				email: $email
+				password: $password
+				confirmPassword: $confirmPassword
+			}
+		) {
+			id
+			email
+			username
+			createdAt
+			token
+		}
+	}
+`;
 
 export default Register;
